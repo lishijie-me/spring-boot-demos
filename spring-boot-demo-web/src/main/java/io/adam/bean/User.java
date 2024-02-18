9cae850d93ab44b15a0ea8f453c78e405fb7fcdc
lishijie-me <2424782003@qq.com>
2024-02-18 21:39:46 +0800

    用户登录页面

--- spring-boot-demo-web/src/main/java/io/adam/bean/User.java
+++ spring-boot-demo-web/src/main/java/io/adam/bean/User.java
@@ -1,11 +1,26 @@
 package io.adam.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author : 成行(lishijie-me)
  * @version : v1.0
- * @createTime : 2024/2/7 18:16
+ * @createTime : 2024/2/18 18:16
  * @description : 用户信息实体类
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class User {
+    private Long id;
+    private Long userId;
     private String userName;
+    private String password;
+    private String phoneNumber;
+    private String eMail;
+    private String address;
 }
---
+++ spring-boot-demo-web/src/main/java/io/adam/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package io.adam.mapper;
+
+/**
+ * @author : 成行(lishijie-me)
+ * @version : v1.0
+ * @createTime : 2024/2/18 19:56
+ * @description : 用户mapper接口
+ */
+public interface UserMapper {
+
+}
