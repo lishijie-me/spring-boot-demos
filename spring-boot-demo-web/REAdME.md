9a96a09c4732e4dcaf236eca61dea79839f15553
lishijie-me <2424782003@qq.com>
2024-02-22 00:05:10 +0800

    登录功能调试完成，登录成功后跳转到列表页面，注意@{}的使用

--- spring-boot-demo-web/REAdME.md
+++ spring-boot-demo-web/REAdME.md
@@ -1,4 +1,5 @@
 [访问首页地址](http://localhost:8060/trade-web/selectOrderAll)
 
 http://localhost:8060/trade-web/selectOrderAll
+## 登录地址
 http://localhost:8060/trade-web/users/login
--- spring-boot-demo-web/src/main/java/io/adam/exception/ControllerExceptionHandler.java
+++ spring-boot-demo-web/src/main/java/io/adam/exception/ControllerExceptionHandler.java
@@ -1,29 +1,29 @@
-package io.adam.exception;
-
-import io.adam.constants.HttpCode;
-import io.adam.resp.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-/**
- * @author : 成行(lishijie-me)
- * @version : v1.0
- * @createTime : 2024/2/19 18:36
- * @description : 一句话描述该类的功能
- */
-@RestControllerAdvice
-public class ControllerExceptionHandler {
-
-    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
-
-    /**
-     * 全局异常处理，统一返回状态码
-     */
-    @ExceptionHandler(Exception.class)
-    public Result<String> handleException(Exception e) {
-        logger.error("服务器抛出了异常：{}", e);
-        return new Result<String>(HttpCode.FAILURE, "执行失败", e.getMessage());
-    }
-}
+//package io.adam.exception;
+//
+//import io.adam.constants.HttpCode;
+//import io.adam.resp.Result;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+///**
+// * @author : 成行(lishijie-me)
+// * @version : v1.0
+// * @createTime : 2024/2/19 18:36
+// * @description : 一句话描述该类的功能
+// */
+//@RestControllerAdvice
+//public class ControllerExceptionHandler {
+//
+//    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+//
+//    /**
+//     * 全局异常处理，统一返回状态码
+//     */
+//    @ExceptionHandler(Exception.class)
+//    public Result<String> handleException(Exception e) {
+//        logger.error("服务器抛出了异常：{}", e);
+//        return new Result<String>(HttpCode.FAILURE, "执行失败", e.getMessage());
+//    }
+//}
